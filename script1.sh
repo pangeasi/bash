0de40e3ac9980afbe981e530f0ca4a0edf3c1284
pangeasi <pangeanoteniafronteras@gmail.com>
2018-02-27 09:37:55 +0100

    Delete script1.sh

--- script1.sh
+++
@@ -1,4 +0,0 @@
-mkdir ejercicio2
-cd ejercicio2
-cat /etc/passwd | cut -f3 -d: | tr ':' ' ' | awk '{print $1}' | xargs mkdir -p
-cat /etc/passwd | cut -f1,3 -d: | tr ':' ' ' | awk '{print $2"/"$1}' | xargs touch
